389aed0004f1486d17d7521254b21b02a5c52910
Dmitry Shadrin <dshadrin@expediagroup.com>
2021-12-06 19:58:01 -0800

    Adding dependencies: react-spectrum, axios, storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Provider, defaultTheme, Button} from '@adobe/react-spectrum';
+import { getBreeds } from './DogApi';
 import './App.css';
+import { DogBreedsList } from "./DogBreedsList";
 
 function App() {
+  const buttonClickHandler = async () => {
+    await getBreeds();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider theme={defaultTheme}>
+      <div className="App">
+        <DogBreedsList data="I am a list. Who are you?" />
+        <header className="App-header">
+          <h1>Woof woof</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Button variant="cta" onPress={buttonClickHandler}>Click me...</Button>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
