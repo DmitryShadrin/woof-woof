87dfc57a0c10ed319f604199ecdc007014a71140
Dmitry Shadrin <dshadrin@expediagroup.com>
2021-12-08 13:41:49 -0800

    Improving DogBreedDetail.

--- src/components/DogBreedDetail.tsx
+++ src/components/DogBreedDetail.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
-import {Cell, Column, Header, Image, Row, TableBody, TableHeader, TableView} from '@adobe/react-spectrum'
-import {DogBreedType} from '../models';
+import { get } from 'lodash';
+import { Cell, Column, Flex, Header, Image, Row, TableBody, TableHeader, TableView } from '@adobe/react-spectrum';
+import { DogBreedType } from '../models';
 
 export const DogBreedDetail = (props: DogBreedType) => {
-  const { id, name, image, bred_for } = props;
+  const { name, image } = props;
 
-  return <>
-    <Header>Breed: {name}</Header>
-    <TableView>
-      <TableHeader>
-        <Column>Parameter</Column>
-        <Column>Value</Column>
-      </TableHeader>
-      <TableBody>
-        <Row>
-          <Cell>Id</Cell>
-          <Cell>{id}</Cell>
-        </Row>
-        <Row>
-          <Cell>Name</Cell>
-          <Cell>{name}</Cell>
-        </Row>
-        <Row>
-          <Cell>Bred for</Cell>
-          <Cell>{bred_for}</Cell>
-        </Row>
-      </TableBody>
-    </TableView>
-    <div>{JSON.stringify(props)}</div>
-    <Image src={image.url} alt={name} maxWidth={400} />
-  </>
+  const getRow = (propName: string, propPath: string, suffix: string = '') => (
+    <Row key={`row-${propPath}`}>
+      <Cell key={`param-${propPath}`}>{propName}</Cell>
+      <Cell key={`value-${propPath}`}>
+        {get(props, propPath)} {suffix}
+      </Cell>
+    </Row>
+  );
+
+  return (
+    <>
+      <Header>Breed: {name}</Header>
+      <TableView key={name}>
+        <TableHeader>
+          <Column>Parameter</Column>
+          <Column>Value</Column>
+        </TableHeader>
+        <TableBody>
+          {getRow('Breed', 'name')}
+          {getRow('Breed group', 'breed_group')}
+          {getRow('Bred for', 'bred_for')}
+          {getRow('Weight (imperial)', 'weight.imperial', 'lb')}
+          {getRow('Weight (metric)', 'weight.metric', 'kg')}
+          {getRow('Life span', 'life_span')}
+          {getRow('Temperament', 'temperament')}
+          {getRow('Origin', 'origin')}
+        </TableBody>
+      </TableView>
+      <Flex width="100%" height="400px" justifyContent="center">
+        <Image src={image.url} alt={name} objectFit="cover" />
+      </Flex>
+    </>
+  );
 };
